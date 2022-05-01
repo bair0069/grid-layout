a9c6621e65e7236d714f599377fd5f76273fb6e5
bair0069 <bair0069@algonquinlive.com>
2022-05-01 14:20:35 -0400

    add auto rows

--- main.js
+++ main.js
@@ -1,76 +1,92 @@
-
-const numberOfRows = document.querySelector("#rows")
+const numberOfRows = document.querySelector("#rows");
 const grid = document.querySelector("#grid");
-const numberOfColumns = document.querySelector("#cols")
+const numberOfColumns = document.querySelector("#cols");
 const numOfBoxes = document.querySelector("#boxes");
 const form = document.querySelector(".form");
 const boxHeight = document.querySelector("#height");
 const boxWidth = document.querySelector("#width");
 const gap = document.querySelector("#gap");
-const ratios = document.querySelectorAll(".ratio-choice");
-
-function init (){
-  clearGrid();
-  createGrid(numOfBoxes.value);
-}
-
+const boxRatios = document.querySelectorAll(".ratio-choice");
+const gridAutos = document.querySelectorAll(".grid-auto-choice");
 
 form.addEventListener("change", (e) => {
   e.preventDefault();
   clearGrid();
-  getRadioSelection();
+  getBoxRatioSelection();
   createGrid(numOfBoxes.value);
 });
 
-function clearGrid (){
-  console.log("init fired")
+function clearGrid() {
   grid.innerHTML = "";
   grid.classList.remove("created-grid");
 }
 
-function getRadioSelection (){
-  let ratioList = Array.from(ratios);
-  let radioChoice =ratioList.filter(radio => {
-    radio.addEventListener('click',(e)=>{
-      let selection = e.target.closest('.ratio-choice')
+function getBoxRatioSelection() {
+  let ratioList = Array.from(boxRatios);
+  let radioChoice = ratioList.filter((radio) => {
+    radio.addEventListener("click", (e) => {
+      let selection = e.target.closest(".ratio-choice");
       selection.checked = true;
-      
     });
     return radio.checked;
-  })
-  console.log(radioChoice[0].value)
-  let choice = radioChoice[0].value;
-  switch (choice) {
-    case "1:1":
-      boxHeight.disabled=false;
-      boxWidth.disabled=true;
-      return boxWidth.value = boxHeight.value
+  });
+  if(radioChoice.length==0)   {
+    boxHeight.value = 10;
+    boxWidth.value = 10;
+  } else {
+    let choice = radioChoice[0].value;
+    switch (choice) {
+      case "1:1":
+        boxHeight.disabled = false;
+        boxWidth.disabled = true;
+        return (boxWidth.value = boxHeight.value);
 
-    case "3:4":
-      boxHeight.disabled=false;
-      boxWidth.disabled=true;
-      return boxWidth.value = boxHeight.value * 3/4
-    case "4:3":
-      boxHeight.disabled=false;
-      boxWidth.disabled=true;
-      return boxWidth.value = boxHeight.value * 4/3
-    case "16:9":
-      boxWidth.disabled=false;
-      boxHeight.disabled=true;
-      return boxHeight.value = boxWidth.value * 9/16
-    case "Custom":
-      boxHeight.disabled=false;
-      boxWidth.disabled=false;
-      return
-    default:
-      return
-}
+      case "3:4":
+        boxHeight.disabled = false;
+        boxWidth.disabled = true;
+        return (boxWidth.value = (boxHeight.value * 3) / 4);
+      case "4:3":
+        boxHeight.disabled = false;
+        boxWidth.disabled = true;
+        return (boxWidth.value = (boxHeight.value * 4) / 3);
+      case "16:9":
+        boxWidth.disabled = false;
+        boxHeight.disabled = true;
+        return (boxHeight.value = (boxWidth.value * 9) / 16);
+      case "Custom":
+        boxHeight.disabled = false;
+        boxWidth.disabled = false;
+        return;
+      default:
+        return;
+    }
+  }
 }
 
+function getGridAutoSelection() {
+  let autoList = Array.from(gridAutos);
+  let radioChoice = autoList.filter((radio) => {
+    radio.addEventListener("click", (e) => {
+      let selection = e.target.closest(".grid-auto-choice");
+      selection.toggleAttribute("checked");
+    });
+    return radio
+  });
+
+  radioChoice.forEach((choice) => {
+    if(choice.checked) {
+      if (choice.name == "rows-auto") {
+        grid.style.gridTemplateRows = 'auto';
+        numberOfRows.disabled = choice.checked
+      }
+    }
+    else {
+      numberOfRows.disabled = false;
+    }
+  });
+}
 
 function createGrid(boxes) {
-
-  console.log("adding", boxes, "boxes");
   for (let i = 0; i < boxes; i++) {
     let div = document.createElement("div");
     div.classList.add("grid-item");
@@ -80,14 +96,16 @@ function createGrid(boxes) {
 
   grid.style.gridTemplateColumns = `repeat(${numberOfColumns.value}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${numberOfRows.value}, 1fr)`;
+  getGridAutoSelection();
   grid.style.gridGap = `${gap.value}rem`;
 
   let gridItems = document.querySelectorAll(".grid-item");
-  if(gridItems) {
-    gridItems.forEach(item => {
+  if (gridItems) {
+    gridItems.forEach((item) => {
       item.style.height = `${boxHeight.value}rem`;
       item.style.width = `${boxWidth.value}rem`;
     });
+  } else {
+    (boxHeight.display = "none"), (boxWidth.display = "none");
   }
-  else {boxHeight.display = "none", boxWidth.display = "none"}
 }
